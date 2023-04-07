6a99762e5d52276ffde7080a4614331f3f1d2629
Noc <t.gelotte@gmail.com>
2023-04-07 18:19:41 +0200

    radio module updated

--- rtl/radio.vhd
+++ rtl/radio.vhd
@@ -9,22 +9,30 @@ entity radio is
     port(
         clk  : in std_logic;
         rst : in STD_LOGIC;
-        channel_1 : in STD_LOGIC;
+        ch1 : in STD_LOGIC;
         signal_1 : out STD_LOGIC_VECTOR(9 downto 0);
-        multiplier : in unsigned(9 downto 0)
-        
+        calibration : in STD_LOGIC;
+        calibration_done : out STD_LOGIC
     );
 end entity radio;
 
 architecture rtl of radio is
     
-    constant micro_second : real := 1.0;
-    constant clock_divisor : positive := positive(frequency_mhz / micro_second);
+    signal micro_second : real := 1.0;
+    signal clock_divisor : positive := positive(frequency_mhz / micro_second);
     signal clock_divider_counter : natural range 0 to clock_divisor + 1 := 0;
     signal radio_clock : STD_LOGIC := '0';
     
-    signal channel_1_counter : integer := 0;
-    signal channel_1_last_val : STD_LOGIC;
+    
+    -- Signal 1
+    
+    signal ch1_min : integer := 1000;
+    signal ch1_max : integer := 2000;
+    
+    signal ch1_scaling_factor : integer;
+    
+    signal ch1_counter : integer := 0;
+    signal ch1_last_val : STD_LOGIC;
     
 begin
 
@@ -41,23 +49,48 @@ begin
     end process;
     
     process(clk, radio_clock, rst)
-        variable tmp : unsigned (19 downto 0);
     begin
         
         
-        if rising_edge(clk) then            
+        if rising_edge(clk) then
             
-            if radio_clock = '1' AND channel_1 = '1' then                
-                channel_1_counter <= channel_1_counter + 1;
+            if calibration = '1' then
+                signal_1 <= std_logic_vector(to_unsigned(500, signal_1'length));
+                
+                if ch1_counter < ch1_min then
+                    ch1_min <= ch1_counter;
+                end if;
+                if ch1_counter > ch1_max then
+                    ch1_max <= ch1_counter;
+                end if;
+                
+                
+                -- dividend * 2^5
+                
+                for i in 0 to 4 loop
+                    if dividend > divisor then
+                        dividend <= dividend - divisor;
+                        scaling_counter <= scaling_counter + 1;
+                    end if;
+                end loop;
+                
+                -- scaling_factor / 2^n
+                
+            else
+                
             end if;
             
-            if channel_1_last_val = '1' AND channel_1 = '0' then
-                tmp := to_unsigned(channel_1_counter - 1000, signal_1'length)*multiplier;
-                signal_1 <= std_logic_vector(tmp(19 downto 10));
-                channel_1_counter <= 0;
+            if radio_clock = '1' AND ch1 = '1' then
+                ch1_counter <= ch1_counter + 1;
+            end if;           
+            
+            if ch1_last_val = '1' AND ch1 = '0' then
+                
+                signal_1 <= std_logic_vector(to_unsigned((ch1_counter - ch1_min) * , signal_1'length));
+                ch1_counter <= 0;
             end if;
             
-            channel_1_last_val <= channel_1;
+            ch1_last_val <= ch1;
 
             
             
